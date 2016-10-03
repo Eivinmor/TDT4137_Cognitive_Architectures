9704075d99e625d44dd11ef9d49252bc63d7499f
Eivind Morch <eivinmor@stud.ntnu.no>
2016-10-03 17:35:06 +0200

    not done

--- Assignment3/Part_b.py
+++ Assignment3/Part_b.py
@@ -1,6 +1,5 @@
-import math
 
-distance_dictionary = {
+distance_dict = {
     "VerySmall": {"type": "reverse_grade", "x0": 1, "x1": 2.5},
     "Small": {"type": "triangle", "x0": 1.5, "x1": 3, "x2": 4.5},
     "Perfect": {"type": "triangle", "x0": 3.5, "x1": 5, "x2": 6.5},
@@ -8,13 +7,35 @@ distance_dictionary = {
     "VeryBig": {"type": "grade", "x0": 7.5, "x1": 9}
 }
 
-delta_dictionary = {
+delta_dict = {
     "ShrinkingFast": {"type": "reverse_grade", "x0": -4, "x1": -2.5},
     "Shrinking": {"type": "triangle", "x0": -3.5, "x1": -2, "x2": -0.5},
     "Stable": {"type": "triangle", "x0": -1.5, "x1": 0, "x2": 1.5},
     "Growing": {"type": "triangle", "x0": 0.5, "x1": 2, "x2": 3.5},
     "GrowingFast": {"type": "grade", "x0": 2.5, "x1": 4},
 }
+
+action_dict = {
+    "BrakeHard": {"type": "reverse_grade", "x0": -8, "x1": -5},
+    "SlowDown": {"type": "triangle", "x0": -7, "x1": -4, "x2": -1},
+    "None": {"type": "triangle", "x0": -3, "x1": 0, "x2": 3},
+    "SpeedUp": {"type": "triangle", "x0": 1, "x1": 4, "x2": 7},
+    "FloorIt": {"type": "grade", "x0": 5, "x1": 8},
+}
+
+distance_value = 3.7
+delta_value = 1.2
+
+
+def mamdani_resonate():
+    return None
+
+def rule1():
+    small_fuzzy = triangle(small)
+    growing_fuzzy =
+    action_dict["None"]["fuzzy"] = min(small_fuzzy, growing_fuzzy)
+
+
 
 def triangle(position, x0, x1, x2, clip):
     value = 0.0
@@ -25,6 +46,7 @@ def triangle(position, x0, x1, x2, clip):
     if value > clip:
             value = clip;
     return value
+
 
 def grade(position, x0, x1, clip):
     value = 0.0
