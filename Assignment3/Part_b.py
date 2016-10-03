5ad8fd2e4b4b9f8d4ed099a89cec15a312b3b817
Eivind Morch <eivinmor@stud.ntnu.no>
2016-10-03 16:53:29 +0200

    Added distance dict

---
+++ Assignment3/Part_b.py
@@ -0,0 +1,43 @@
+import math
+
+distance_dictionary = {
+    "VerySmall": {"type": "reverse_grade", "x0": 1, "x1": 2.5},
+    "Small": {"type": "triangle", "x0": 1.5, "x1": 3, "x2": 4.5},
+    "Perfect": {"type": "triangle", "x0": 3.5, "x1": 5, "x2": 6.5},
+    "Big": {"type": "triangle", "x0": 5.5, "x1": 7, "x2": 8.5},
+    "VeryBig": {"type": "grade", "x0": 7.5, "x1": 9}
+}
+
+def triangle(position, x0, x1, x2, clip):
+    value = 0.0
+    if position >= x0 and position <= x1:
+            value = (position - x0) / (x1 - x0)
+    elif position >= x1 and position <= x2:
+            value = (x2 - position) / (x1 - x0)
+    if value > clip:
+            value = clip;
+    return value
+
+def grade(position, x0, x1, clip):
+    value = 0.0
+    if position >= x1:
+        value = 1.0
+    elif position <= x0:
+        value = 0.0
+    else:
+        value = (position - x0) / (x1 - x0)
+    if value > clip:
+        value = clip
+    return value
+
+
+def reverse_grade(position, x0, x1, clip):
+    value = 0.0
+    if position <= x0: value = 1.0
+    elif position >= x1:
+        value = 0.0
+    else:
+        value = (x1 - position) / (x1 - x0)
+    if value > clip:
+        value = clip;
+    return value
