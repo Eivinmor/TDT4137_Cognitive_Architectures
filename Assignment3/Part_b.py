7ae714265652a52134d5f63109ad194a1711a730
Eivind Morch <Eivind Morch>
2016-10-10 05:14:01 +0200

    Added all functionality and prettyprint

--- Assignment3/Part_b.py
+++ Assignment3/Part_b.py
@@ -1,73 +1,143 @@
+import sys
+this_module = sys.modules[__name__]
 
 distance_dict = {
-    "VerySmall": {"type": "reverse_grade", "x0": 1, "x1": 2.5},
-    "Small": {"type": "triangle", "x0": 1.5, "x1": 3, "x2": 4.5},
-    "Perfect": {"type": "triangle", "x0": 3.5, "x1": 5, "x2": 6.5},
-    "Big": {"type": "triangle", "x0": 5.5, "x1": 7, "x2": 8.5},
-    "VeryBig": {"type": "grade", "x0": 7.5, "x1": 9}
+    "VerySmall": {"type": "reverse_grade", "x": [1, 2.5]},
+    "Small": {"type": "triangle", "x": [1.5, 3, 4.5]},
+    "Perfect": {"type": "triangle", "x": [3.5, 5, 6.5]},
+    "Big": {"type": "triangle", "x": [5.5, 7, 8.5]},
+    "VeryBig": {"type": "grade", "x": [7.5, 9]}
 }
 
 delta_dict = {
-    "ShrinkingFast": {"type": "reverse_grade", "x0": -4, "x1": -2.5},
-    "Shrinking": {"type": "triangle", "x0": -3.5, "x1": -2, "x2": -0.5},
-    "Stable": {"type": "triangle", "x0": -1.5, "x1": 0, "x2": 1.5},
-    "Growing": {"type": "triangle", "x0": 0.5, "x1": 2, "x2": 3.5},
-    "GrowingFast": {"type": "grade", "x0": 2.5, "x1": 4},
+    "ShrinkingFast": {"type": "reverse_grade", "x": [-4, -2.5]},
+    "Shrinking": {"type": "triangle", "x": [-3.5, -2, -0.5]},
+    "Stable": {"type": "triangle", "x": [-1.5, 0, 1.5]},
+    "Growing": {"type": "triangle", "x": [0.5, 2, 3.5]},
+    "GrowingFast": {"type": "grade", "x": [2.5, 4]},
 }
 
 action_dict = {
-    "BrakeHard": {"type": "reverse_grade", "x0": -8, "x1": -5},
-    "SlowDown": {"type": "triangle", "x0": -7, "x1": -4, "x2": -1},
-    "None": {"type": "triangle", "x0": -3, "x1": 0, "x2": 3},
-    "SpeedUp": {"type": "triangle", "x0": 1, "x1": 4, "x2": 7},
-    "FloorIt": {"type": "grade", "x0": 5, "x1": 8},
+    "BrakeHard": {"type": "reverse_grade", "x": [-8, -5], "cut": 1.0},
+    "SlowDown": {"type": "triangle", "x": [-7, -4, -1],  "cut": 1.0},
+    "None": {"type": "triangle", "x": [-3, 0, 3],  "cut": 1.0},
+    "SpeedUp": {"type": "triangle", "x": [1, 4, 7],  "cut": 1.0},
+    "FloorIt": {"type": "grade", "x": [5, 8],  "cut": 1.0},
 }
 
 distance_value = 3.7
 delta_value = 1.2
 
+action_range_min = -10
+action_range_max = 10
+
 
 def mamdani_resonate():
-    return None
+    action_dict["None"]["cut"] = AND(fuzzy("Small"), fuzzy("Growing"))
+    action_dict["SlowDown"]["cut"] = AND(fuzzy("Small"), fuzzy("Stable"))
+    action_dict["SpeedUp"]["cut"] = AND(fuzzy("Perfect"), fuzzy("Growing"))
+    action_dict["FloorIt"]["cut"] = AND(fuzzy("VeryBig"), OR(NOT(fuzzy("Growing")), NOT(fuzzy("GrowingFast"))))
+    action_dict["BrakeHard"]["cut"] = fuzzy("VerySmall")
 
-def rule1():
-    small_fuzzy = triangle(small)
-    growing_fuzzy =
-    action_dict["None"]["fuzzy"] = min(small_fuzzy, growing_fuzzy)
+    print("\nCut values:")
+    for key, dict in action_dict.items():
+        print("{:<12}".format(key), round(dict["cut"], 2))
+    print("\n")
+
+    dividend = 0
+    divisor = 0
+    for position in range(action_range_min, action_range_max + 1):
+        max_action, max_value = get_max_action(position)
+        dividend += position * max_value
+        divisor += max_value
+        print("{:>3}".format(position), "  ", end="")
+        print("{:<8}".format(max_action), "   ", end="")
+        print("{:<6}".format(round(max_value, 2)), end="\t")
+        i = 0
+        while i < max_value*30:
+            print("-", end="")
+            i += 1
+        print("*")
+
+    cog = dividend/divisor
+    print("\nCOG: ", cog)
+    cog_action, cog_value = get_max_action(cog)
+    print("Action: ", cog_action)
 
 
+def get_max_action(position):
+    cur_max_action = "-"
+    cur_max_value = 0
+    for key in action_dict.keys():
+        value = get_value(key, position)
+        if value > cur_max_value:
+            cur_max_action = key
+            cur_max_value = value
+    return cur_max_action, cur_max_value
 
-def triangle(position, x0, x1, x2, clip):
-    value = 0.0
-    if position >= x0 and position <= x1:
-            value = (position - x0) / (x1 - x0)
-    elif position >= x1 and position <= x2:
-            value = (x2 - position) / (x1 - x0)
-    if value > clip:
-            value = clip;
+def fuzzy(var):
+    if var in distance_dict:
+        position = distance_value
+        var_dict = distance_dict[var]
+    elif var in delta_dict:
+        position = delta_value
+        var_dict = delta_dict[var]
+    function = getattr(this_module, var_dict["type"])
+    fuzzy_value = function(position, var_dict["x"], 1)
+    return fuzzy_value
+
+def get_value(action, position):
+    function = getattr(this_module, action_dict[action]["type"])
+    value = function(position, action_dict[action]["x"], action_dict[action]["cut"])
     return value
 
 
-def grade(position, x0, x1, clip):
+def AND(a, b):
+    return min(a, b)
+
+
+def OR(a, b):
+    return max(a, b)
+
+
+def NOT(a):
+    return 1-a
+
+
+def triangle(position, x, cut):
     value = 0.0
-    if position >= x1:
+    if position >= x[0] and position <= x[1]:
+            value = (position - x[0]) / (x[1] - x[0])
+    elif position >= x[1] and position <= x[2]:
+            value = (x[2] - position) / (x[1] - x[0])
+    if value > cut:
+            value = cut
+    return value
+
+
+def grade(position, x, cut):
+    value = 0.0
+    if position >= x[1]:
         value = 1.0
-    elif position <= x0:
+    elif position <= x[0]:
         value = 0.0
     else:
-        value = (position - x0) / (x1 - x0)
-    if value > clip:
-        value = clip
+        value = (position - x[0]) / (x[1] - x[0])
+    if value > cut:
+        value = cut
     return value
 
 
-def reverse_grade(position, x0, x1, clip):
+def reverse_grade(position, x, cut):
     value = 0.0
-    if position <= x0: value = 1.0
-    elif position >= x1:
+    if position <= x[0]: value = 1.0
+    elif position >= x[1]:
         value = 0.0
     else:
-        value = (x1 - position) / (x1 - x0)
-    if value > clip:
-        value = clip;
+        value = (x[1] - position) / (x[1] - x[0])
+    if value > cut:
+        value = cut
     return value
+
+
+mamdani_resonate()
